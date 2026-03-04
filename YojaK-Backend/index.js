db3a1f1af869c4f9a18834fe92b646eced7daa56
Vivek Yadav <vivekyad240706@gmail.com>
2026-03-04 05:52:05 +0530

    Project Setup : Backend Setup Files created

---
+++ YojaK-Backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
